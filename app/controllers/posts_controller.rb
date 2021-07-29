e247be2cbabee15c535b5ce9ff2dd5a552220d98
fumi hagihara <7676.fmhghr@gmail.com>
2021-07-29 23:31:56 +0900

    blog_paramsメソッドを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end
   def create
-    Post.create(params.require(:post).permit(:content))
+    Post.create(post_params)
     redirect_to new_post_path
+  end
+  def blog_params
+    Post.create(params.require(:post).permit(:content))
   end
 end
