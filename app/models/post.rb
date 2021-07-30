011aea5491743664faf732c56e7a92076d016b4a
fumi hagihara <7676.fmhghr@gmail.com>
2021-07-30 10:29:25 +0900

    エラーメッセージの表示を実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
-  validates :content, length: { minimum: 1, maximum: 140}
+  validates :content, presence: true,
+                      length: { minimum: 1, maximum: 140}
 end
